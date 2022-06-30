d50d794030ed6d340a56998c410abf0b9678f215
Andrea Visentin <101669117+TheCrow28@users.noreply.github.com>
2022-06-30 18:38:33 +0200

    Add files via upload

---
+++ API - Interceptor Middleware 01/src/main/java/co/develhope/interceptorsmiddleware1/controllers/BasicController.java
@@ -0,0 +1,16 @@
+package co.develhope.interceptorsmiddleware1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/time")
+public class BasicController {
+
+    @GetMapping
+    public LocalDate getDate(){ return java.time.LocalDate.now();
+    }
+}
---
+++ API - Interceptor Middleware 01/src/main/java/co/develhope/interceptorsmiddleware1/controllers/LegacyController.java
@@ -0,0 +1,14 @@
+package co.develhope.interceptorsmiddleware1.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/legacy")
+public class LegacyController {
+
+    public String legacy(){
+        return "This is just old code";
+    }
+}
