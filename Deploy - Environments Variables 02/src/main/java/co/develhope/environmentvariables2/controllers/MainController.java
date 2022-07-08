8e00a88ccde2a8bfdbdcebc86438f21ba421b9e3
Andrea Visentin <101669117+TheCrow28@users.noreply.github.com>
2022-07-08 16:52:49 +0200

    Add files via upload

---
+++ Deploy - Environments Variables 02/src/main/java/co/develhope/environmentvariables2/controllers/MainController.java
@@ -0,0 +1,19 @@
+package co.develhope.environmentvariables2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String getMessage(){
+        String welcomeMsg = environment.getProperty("myCustomEnvs.welcomeMsg");
+        return welcomeMsg;
+    }
+}
